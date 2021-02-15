2e653b5d23a6f8f0366fdab61e2f5d471fb47206
rafael-domingo <38447983+rafael-domingo@users.noreply.github.com>
2021-02-14 22:51:44 -0600

    Create workspace file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What's For Lunch?
         </p>
         <a
           className="App-link"
